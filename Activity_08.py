0519fd5473dcce7d1939384a3df85230d1f7341b
Umgoel <76695806+Umgoel@users.noreply.github.com>
2021-08-28 16:38:58 +0530

    Update Activity_08.py

---
+++ Activity_08.py
@@ -0,0 +1,3 @@
+1. An interpreted language is one where the entire language is executed line by line
+2. an interpreted language is translated line by line whereas a compiled language is translated at once
+3. C
