f44db621e7c60d0e203f036f08a196ea03d0ea94
Umgoel <76695806+Umgoel@users.noreply.github.com>
2021-08-28 17:07:46 +0530

    Update Activity_07.py

--- Activity_07.py
+++ Activity_07.py
@@ -1,7 +1,7 @@
 #Accept two numbers from the user and find output their sum in the given format
 s=input("Enter 2 no.s : ")
-ls=s.split()
-sum=0
-for i in ls:
-     sum+=int(i)
-print(sum)
+#ls=s.split()
+#sum=0
+#for i in ls:
+#     sum+=int(i)
+print(sum(ls,0))
